15b659f01620a66ba9c4ec744b2c01cd80d4c87f
Jhoan Caramantin <jhoan1915@gmail.com>
2021-05-10 17:24:30 -0500

    feat: Inicio AdminComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContainerAppComponent } from './components/pages/container-app/container-app.component';
 import { PostComponent } from './components/posts/post/post.component';
 
 const routes: Routes = [
-  {path:'',redirectTo:'/home',pathMatch:'full'},
-  { path: 'home',
-  loadChildren: () =>
-  import('./components/page/home/home.module').then(
-    m => m.HomeModule)
+  {
+    path:'',component:ContainerAppComponent,
+    children:[
+      {
+         path: 'home',
+      loadChildren: () =>
+      import('./components/page/home/home.module').then(
+        m => m.HomeModule)
+      },
+      {
+        path: 'post/:id', component:PostComponent
+      },
+      {
+        path: 'about', loadChildren: () => import('./components/pages/about/about.module').then(m => m.AboutModule)
+      },
+      {
+        path:'',
+        redirectTo:'home',
+        pathMatch:'full'
+
+      }
+    ]
   },
-  { path: 'post/:id', component:PostComponent },
-  { path: 'about', loadChildren: () => import('./components/pages/about/about.module').then(m => m.AboutModule) },
-  { path: 'admin', loadChildren: () => import('./components/admin/admin.module').then(m => m.AdminModule) },
+      { path: 'admin', loadChildren: () => import('./components/admin/admin.module').then(m => m.AdminModule) },
   { path: 'login', loadChildren: () => import('./components/auth/login/login.module').then(m => m.LoginModule) }
 
 ];
