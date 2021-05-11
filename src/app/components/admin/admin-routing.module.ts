e5f050f796d2fd5b1de0a1f2397281bb4dfe837e
Jhoan Caramantin <jhoan1915@gmail.com>
2021-05-11 17:10:13 -0500

    feat:PProfile&Listposts in AdminComponent

--- src/app/components/admin/admin-routing.module.ts
+++ src/app/components/admin/admin-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
+import { ListPostsModule } from '../posts/list-posts/list-posts.module';
 
-const routes: Routes = [{ path: '', component: AdminComponent }];
+const routes: Routes = [
+  {
+    path: '',
+   component: AdminComponent,
+    children:[
+      {
+        path:'posts',
+        loadChildren: () =>
+          import('../posts/list-posts/list-posts.module').then(
+           m => m.ListPostsModule
+        )
+
+      }
+      ,
+       { path: 'profile',
+        loadChildren: () =>
+        import('./profile/profile.module').then(
+          m => m.ProfileModule) }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
+  public opened =false;
   constructor() { }
 
   ngOnInit(): void {
