5e6140452c605930753db9dc21cce817a8f91a1c
Jhoan Caramantin <jhoan1915@gmail.com>
2021-05-25 16:53:18 -0500

    deploy netlify

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDOFcNdL_bmRA1lPaFzH0fc5ksG4Np66a0",
+    authDomain: "hololive-fire.firebaseapp.com",
+    projectId: "hololive-fire",
+    storageBucket: "hololive-fire.appspot.com",
+    messagingSenderId: "860072874757",
+    appId: "1:860072874757:web:d8309ea3fc39afbc791d6c",
+    measurementId: "G-ZHN0L5P631"
+  },
 };
